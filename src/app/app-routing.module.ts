3f57b29fee926dc4843bff5b05ced058f8e8f519
SaLaMaNdeR-81 <a.mahdimahdi1381@gmail.com>
2024-09-04 16:51:26 +0330

    Update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,8 +6,8 @@ import { PageStatus } from './Page/Status/Status.Page';
 const routes: Routes = [
   // { path: 'tag/:tag', component: PageProduct },
   // { path: 'product/:id', component: PageProduct },
-  { path: 'status', component: PageStatus },
-  { path: 'home', component: PageHome },
+  { path: 'status', component: PageStatus  , pathMatch: 'full'},
+  { path: 'home', component: PageHome , pathMatch: 'full'},
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: '**', component: PageHome },
 ];
